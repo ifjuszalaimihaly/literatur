0bfcc9fbf4edc61b958f58f58e839f395a50997f
ifjuszalaimihaly@gmail.com <ifjuszalaimihaly@gmail.com>
2017-07-07 20:27:13 +0200

    added facebook comment section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,35 +1,43 @@
 <!doctype html>
 <html lang="hu">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Szalai Mihály írói oldala</title>
+  <title>Szalai Mihály írói oldala</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        @section("stylesheet")
-        @show
-    </head>
-    <body>
-       <div class="jumbotron"><!-- megfelelőt találni -->
-           <h1>Szalai Mihály írói oldala</h1>
-       </div>
-        @if(Auth::check())
-        @include('partials.adminnav')
-        @endif
-       <div class="container">
-          @section("content")
-          @show
-       </div>
-       <script
-              src="https://code.jquery.com/jquery-3.2.1.min.js"
-              integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-              crossorigin="anonymous"></script>
-       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-       @section('script')
-       @show
-    </body>
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  @section("stylesheet")
+  @show
+</head>
+<body>
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/hu_HU/sdk.js#xfbml=1&version=v2.9&appId=906150422847603";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+  <div class="jumbotron"><!-- megfelelőt találni -->
+    <h1>Szalai Mihály írói oldala</h1>
+  </div>
+  @if(Auth::check())
+  @include('partials.adminnav')
+  @endif
+  <div class="container">
+    @section("content")
+    @show
+  </div>
+  <script
+  src="https://code.jquery.com/jquery-3.2.1.min.js"
+  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+  crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  @section('script')
+  @show
+</body>
 </html>
--- resources/views/layouts/singlepost.blade.php
+++ resources/views/layouts/singlepost.blade.php
@@ -3,4 +3,5 @@
 	<h1>{{$post->title}}</h1>
 	<p>{{ $post->content }}</p>
 	<legend>{{ $post->city }}, {{ $post->written_at }}</legend>
+	<div class="fb-comments" data-href="{{ Request::url() }}" data-numposts="5"></div>
 @endsection
